d6473cea164799a7ff1c38502a0fb87bcba37f8f
NagylaMota <42254697+NagylaMota@users.noreply.github.com>
2020-06-25 20:28:37 -0300

    Revert "teclado ok"
    This reverts commit f9c286b926b323dc5a681fcb6a33ca6b0f699575.

--- app_aula/lib/main.dart
+++ app_aula/lib/main.dart
@@ -39,173 +39,167 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          SingleChildScrollView(
-            // child: Container(
-            child: Column(
-              children: <Widget>[
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height / 2.5,
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [Color(0xFF6495ED), Color(0xFF4169E1)],
-                      ),
-                      borderRadius:
-                          BorderRadius.only(bottomLeft: Radius.circular(90))),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Spacer(),
-                      Align(
-                        alignment: Alignment.center,
-                        child: Container(
-                            height: 150,
-                            width: 150,
-                            child: Image.asset('imagens/drfofo.png',
-                                alignment: Alignment.center)),
-                      ),
-                      Spacer(),
-                    ],
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2.5,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [Color(0xFF6495ED), Color(0xFF4169E1)],
                   ),
-                ),
-                Container(
-                  height: MediaQuery.of(context).size.height / 2,
-                  width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.only(top: 62),
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        width: MediaQuery.of(context).size.width / 1.2,
-                        height: 45,
-                        padding: EdgeInsets.only(
-                            top: 4, left: 16, right: 16, bottom: 4),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(50)),
-                            color: Colors.white,
-                            boxShadow: [
-                              BoxShadow(color: Colors.black12, blurRadius: 5)
-                            ]),
-                        child: TextField(
-                          onChanged: (text) {
-                            email = text;
-                          },
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            icon: Icon(
-                              Icons.account_circle,
-                              color: Colors.grey,
-                            ),
-                            hintText: 'Usuario',
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width / 1.2,
-                        height: 45,
-                        margin: EdgeInsets.only(top: 32),
-                        padding: EdgeInsets.only(
-                            top: 4, left: 16, right: 16, bottom: 4),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(50)),
-                            color: Colors.white,
-                            boxShadow: [
-                              BoxShadow(color: Colors.black12, blurRadius: 5)
-                            ]),
-                        child: TextField(
-                          obscureText: true,
-                          onChanged: (text) {
-                            password = text;
-                          },
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            icon: Icon(
-                              Icons.vpn_key,
-                              color: Colors.grey,
-                            ),
-                            hintText: 'Senha',
-                          ),
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: Padding(
-                          padding: const EdgeInsets.only(right: 15),
-                          child: FlatButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => Cadastro()),
-                              );
-                            },
-                            child: Text(
-                              'Não tem uma conta?',
-                              style: TextStyle(color: Colors.red),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Spacer(),
-                      Container(
-                        alignment: Alignment.center,
-                        margin: EdgeInsets.fromLTRB(21, 28, 21, 0),
-                        child: MaterialButton(
-                          minWidth: double.infinity,
-                          height: 48,
-                          child: Text(
-                            'Entrar',
-                            style: TextStyle(
-                                fontSize: 14,
-                                color: Colors.white,
-                                fontFamily: "Roboto"),
-                          ),
-                          color: HexColor('#4169E1'),
-                          //!ON CLICK DO BOTÃO
-                          onPressed: () async {
-                            try {
-                              FirebaseUser user = (await FirebaseAuth.instance
-                                      .signInWithEmailAndPassword(
-                                          email: email.trim(),
-                                          password: password.trim()))
-                                  .user;
-
-                              if (user != null) {
-                                print('deu certo');
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => Listahospitais()),
-                                );
-                              }
-                            } catch (e) {
-                              print("deu errado");
-                              return AlertDialog(
-                                title: Text("Usuario ou senha incorretos"),
-                                actions: <Widget>[
-                                  FlatButton(
-                                    child: Text("OK"),
-                                    onPressed: () {},
-                                  )
-                                ],
-                              );
-                            }
-                          },
-                          shape: new RoundedRectangleBorder(
-                            borderRadius: new BorderRadius.circular(40.0),
-                          ),
-                        ),
-                      ),
-                    ],
+                  borderRadius:
+                      BorderRadius.only(bottomLeft: Radius.circular(90))),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Spacer(),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                        height: 150,
+                        width: 150,
+                        child: Image.asset('imagens/drfofo.png',
+                            alignment: Alignment.center)),
                   ),
-                )
-              ],
+                  Spacer(),
+                ],
+              ),
             ),
-          )
-        ],
+            Container(
+              height: MediaQuery.of(context).size.height / 2,
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.only(top: 62),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    width: MediaQuery.of(context).size.width / 1.2,
+                    height: 45,
+                    padding:
+                        EdgeInsets.only(top: 4, left: 16, right: 16, bottom: 4),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(color: Colors.black12, blurRadius: 5)
+                        ]),
+                    child: TextField(
+                      onChanged: (text) {
+                        email = text;
+                      },
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        icon: Icon(
+                          Icons.account_circle,
+                          color: Colors.grey,
+                        ),
+                        hintText: 'Usuario',
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width / 1.2,
+                    height: 45,
+                    margin: EdgeInsets.only(top: 32),
+                    padding:
+                        EdgeInsets.only(top: 4, left: 16, right: 16, bottom: 4),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(color: Colors.black12, blurRadius: 5)
+                        ]),
+                    child: TextField(
+                      obscureText: true,
+                      onChanged: (text) {
+                        password = text;
+                      },
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        icon: Icon(
+                          Icons.vpn_key,
+                          color: Colors.grey,
+                        ),
+                        hintText: 'Senha',
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 15),
+                      child: FlatButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Cadastro()),
+                          );
+                        },
+                        child: Text(
+                          'Não tem uma conta?',
+                          style: TextStyle(color: Colors.red),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                  Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.fromLTRB(21, 28, 21, 0),
+                    child: MaterialButton(
+                      minWidth: double.infinity,
+                      height: 48,
+                      child: Text(
+                        'Entrar',
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                            fontFamily: "Roboto"),
+                      ),
+                      color: HexColor('#4169E1'),
+                      //!ON CLICK DO BOTÃO
+                      onPressed: () async {
+                        try {
+                          FirebaseUser user = (await FirebaseAuth.instance
+                                  .signInWithEmailAndPassword(
+                                      email: email.trim(),
+                                      password: password.trim()))
+                              .user;
+
+                          if (user != null) {
+                            print('deu certo');
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Listahospitais()),
+                            );
+                          }
+                        } catch (e) {
+                          print("deu errado");
+                          return AlertDialog(
+                            title: Text("Usuario ou senha incorretos"),
+                            actions: <Widget>[
+                              FlatButton(
+                                child: Text("OK"),
+                                onPressed: () {},
+                              )
+                            ],
+                          );
+                        }
+                      },
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(40.0),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
